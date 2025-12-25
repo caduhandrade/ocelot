4ed77539a826d2fb1df98296083696d2954a8e04
Carlos Eduardo Lopes de Andrade <carlos.landrade@g.globo>
2025-12-25 20:49:17 -0300

    settings

--- ApiGateway/obj/Debug/net10.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net10.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42affb6b9d0ddb5b1963bcc34eaa58457468915e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
